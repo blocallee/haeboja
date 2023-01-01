5edda03d8feb996d2a372b558c89abe2db664db5
blocallee <blocallee@gmail.com>
2023-01-02 00:44:40 +0900

    커밋

--- nomadcoder/js/memeMaker/app.js
+++ nomadcoder/js/memeMaker/app.js
@@ -1,3 +1,9 @@
+const fileSelector = document.getElementById("drawing-file-select");
+const fileName = document.getElementById("drawing-file-name");
+const colorName = document.querySelector(".palette-area__color-text");
+const btnSave = document.getElementById("btn-img-save");
+const addTextInput = document.getElementById("drawing-text");
+const fileUpload = document.getElementById("drawing-file");
 const btnModeEraser = document.getElementById("drawing-mode-eraser");
 const btnModeReset = document.getElementById("drawing-mode-reset");
 const btnMode = document.getElementById("drawing-mode");
@@ -8,14 +14,16 @@ const color = document.getElementById("color-range");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
 const colors = [
-  "#fff",
   "#55efc4",
   "#81ecec",
   "#74b9ff",
@@ -52,6 +60,7 @@ function onLineWidthChange(e) {
 function onColorChange(e) {
   ctx.strokeStyle = e.target.value;
   ctx.fillStyle = e.target.value;
+  colorName.innerText = e.target.value;
 }
 
 function onColorClick(e) {
@@ -59,35 +68,74 @@ function onColorClick(e) {
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
   color.value = colorValue;
+  colorName.innerText = colorValue;
 }
 
-function onModeClick(e) {
+function onModeClick() {
   if (isFilling) {
     isFilling = false;
-    btnMode.innerText = "Fill";
+    btnMode.innerText = "채우기";
   } else {
     isFilling = true;
-    btnMode.innerText = "Draw";
+    btnMode.innerText = "그리기";
   }
 }
 
-function onCanvasClick(e) {
+function onCanvasClick() {
   if (isFilling) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 }
 
-function onResetClick(e) {
+function onResetClick() {
   ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  fileSelector.innerText = "이미지 고르기";
+  fileName.innerText = "선택된 파일 없어요";
 }
 
 function onEraserClick(e) {
   ctx.strokeStyle = "#fff";
   isFilling = false;
-  btnMode.innerText = "Fill";
+  btnMode.innerText = "채우기";
 }
 
+function onFileChange(e) {
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  const fileValue = file.name;
+  console.dir(fileValue);
+  const image = new Image(file);
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileName.innerText = fileValue;
+    fileUpload.value = null;
+    fileSelector.innerText = "다시 고르기";
+  };
+}
+
+function onDoubleClick(e) {
+  const text = addTextInput.value;
+
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "30px serif";
+    ctx.fillText(text, e.offsetX, e.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "MyDrawing.png";
+  a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -99,3 +147,7 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 btnMode.addEventListener("click", onModeClick);
 btnModeReset.addEventListener("click", onResetClick);
 btnModeEraser.addEventListener("click", onEraserClick);
+
+fileUpload.addEventListener("change", onFileChange);
+
+btnSave.addEventListener("click", onSaveClick);
