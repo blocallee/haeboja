2197f99e4a5e79ae4a1154e6fa1d465d21f91315
blocallee <blocallee@gmail.com>
2023-02-12 14:45:54 +0900

    섹션3 5주차 완료_몬스터js

---
+++ study/js/ggang_game/coding/code/js/monster.js
@@ -0,0 +1,41 @@
+const pinkMon = {
+  name: "pink_mon",
+  hpValue: 70000,
+  speed: 2,
+  crashDamage: 300,
+};
+
+const yellowMon = {
+  name: "yellow_mon",
+  hpValue: 60000,
+  speed: 2,
+  crashDamage: 300,
+};
+
+const greenMon = {
+  name: "green_mon",
+  hpValue: 50000,
+  speed: 2,
+  crashDamage: 300,
+};
+
+const greenMonBoss = {
+  name: "green_mon_boss",
+  hpValue: 100000,
+  speed: 4,
+  crashDamage: 1000,
+};
+
+const yellowMonBoss = {
+  name: "yellow_mon_boss",
+  hpValue: 130000,
+  speed: 4,
+  crashDamage: 2000,
+};
+
+const pinkMonBoss = {
+  name: "pink_mon_boss",
+  hpValue: 150000,
+  speed: 3,
+  crashDamage: 2500,
+};
